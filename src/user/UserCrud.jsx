cc7e859f9d8d927832be940247d114b02a2acbd3
Mateus Borsoi <borsoimateus@gmail.com>
2023-05-17 09:44:42 -0300

    router

--- src/user/UserCrud.jsx
+++ src/user/UserCrud.jsx
@@ -8,41 +8,64 @@ const HeaderProps = {
   subtitle: "Cadastro de usuários: Incluir, Listar, Alterar e Excluir ",
 };
 
-const baseUrl = 'http://localhost:3003/users'
+const baseUrl = "http://localhost:3003/users";
 
 const inicialState = {
-    user: {name: '', email: ''},
-    list: []
-}
+  user: { name: "", email: "" },
+  list: [],
+};
 
 export default class UserCrud extends Component {
-    state = {...inicialState}
+  state = { ...inicialState };
 
-    clear() {
-        this.setState({user: inicialState.user})
-    }
+  clear() {
+    this.setState({ user: inicialState.user });
+  }
 
-    save() {
-        const user = this.state.user
-        const method = user.id ? 'put' : 'post'
-        const url = user.id ? `${baseUrl}/${user.id}` : baseUrl
-        axios[method](url, user)
-        .then(resp => {
-            const list = this.getUpdatedList(resp.data)
-            this.setState({user: inicialState.user, list})
-        })
-    }
+  save() {
+    const user = this.state.user;
+    const method = user.id ? "put" : "post";
+    const url = user.id ? `${baseUrl}/${user.id}` : baseUrl;
+    axios[method](url, user).then((resp) => {
+      const list = this.getUpdatedList(resp.data);
+      this.setState({ user: inicialState.user, list });
+      list.unshift(user);
+      return list;
+    });
+  }
 
-    getUpdatedList(user){
-        const list = this.state.list.filter()
-    }
+  getUpdatedList(user) {
+    const list = this.state.list.filter((u) => u.id !== user.id);
+  }
 
-    render () {
-        return (
-            <Main {...HeaderProps}>
-                Usuário
-            </Main>
-        )
-    }
+  updateField(event) {
+    const user = { ...this.state.user };
+    user[event.target.name] = event.target.value;
+    this.setState({ user });
+  }
+
+  renderForm() {
+    return (
+      <div className="form">
+        <div className="row">
+          <div className="col-12 col-md-6">
+            <div className="form-group">
+              <label>Nome</label>
+              <input
+                type="text"
+                className="form-control"
+                name="name"
+                value={this.state.user.name}
+                onChange={event => this.updateField(event)}
+              ></input>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  render() {
+    return <Main {...HeaderProps}>Usuário</Main>;
+  }
 }
-
